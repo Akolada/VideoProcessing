a20a217ada550a0bb5df85e61e8cf0ed6a8874b4
Lento <crosssceneofwindff@gmail.com>
2019-02-10 17:20:38 +0900

    update

--- Two-Stream/model.py
+++ Two-Stream/model.py
@@ -42,8 +42,8 @@ class CBR3D_dis(Chain):
         super(CBR3D_dis,self).__init__()
         with self.init_scope():
             self.cpara=L.ConvolutionND(3,in_ch,out_ch,3,1,1,initialW=w)
-            self.cdown=L.ConvolutionND(3,in_ch,out_ch,4,(1,2,2),1,initialW=w)
-            self.cup=L.DeconvolutionND(3,in_ch,out_ch,4,(1,2,2),1,initialW=w)
+            self.cdown=L.ConvolutionND(3,in_ch,out_ch,4,(2,2,2),1,initialW=w)
+            self.cup=L.DeconvolutionND(3,in_ch,out_ch,4,(2,2,2),1,initialW=w)
 
             self.bn0=L.BatchNormalization(out_ch)
 
@@ -100,14 +100,16 @@ class ResBlock_2D(Chain):
 
 class Generator(Chain):
     def __init__(self,base=64):
-        w=initializers.Normal(0.02)
+        w=initializers.Normal(0.01)
         super(Generator,self).__init__()
         with self.init_scope():
+            self.l0 = L.Linear(128, 512*4*4, initialW=w)
             self.cbr3d_0=CBR3D(base*8,base*4,up=True)
             self.cbr3d_1=CBR3D(base*4,base*2,up=True)
             self.cbr3d_2=CBR3D(base*2,base,up=True)
-            self.cbr3d_3=CBR3D(base,3,up=True)
-            self.c_m=CBR3D(base,1,up=True)
+            self.cbr3d_3=CBR3D(base,base,up=True)
+            self.cbr3d_4 = L.Convolution3D(base,3,3,1,1,initialW=w)
+            self.c_m=L.Convolution3D(base,1,3,1,1,initialW=w)
 
             self.cbr2d_0=CBR2D(3,base,down=True)
             self.cbr2d_1=CBR2D(base,base*2,down=True)
@@ -116,10 +118,11 @@ class Generator(Chain):
             self.res0 = ResBlock_2D(base*8, base*8)
             self.res1 = ResBlock_2D(base*8, base*8)
 
-            self.cbr2d_4=CBR2D(base*16,base*4,up=True)
-            self.cbr2d_5=CBR2D(base*8,base*2,up=True)
-            self.cbr2d_6=CBR2D(base*4,base,up=True)
-            self.cbr2d_7=CBR2D(base,3,up=True)
+            self.cbr2d_4=CBR2D(base*8,base*4,up=True)
+            self.cbr2d_5=CBR2D(base*4,base*2,up=True)
+            self.cbr2d_6=CBR2D(base*2,base,up=True)
+            self.cbr2d_7=CBR2D(base,base,up=True)
+            self.cbr2d_8=L.Convolution2D(base,3,3,1,1,initialW=w)
 
     def encode(self,x):
         h=self.cbr2d_0(x)
@@ -132,31 +135,37 @@ class Generator(Chain):
         return h1,h2,h3
 
     def foreground(self,x):
-        x=x.reshape(2,512,1,2,2)
+        x=x.reshape(2,512,1,4,4)
         h=self.cbr3d_0(x)
         h=self.cbr3d_1(h)
-        hm=self.cbr3d_2(h)
-        h=self.cbr3d_3(hm)
+        h=self.cbr3d_2(h)
+        hm=self.cbr3d_3(h)
+        h=self.cbr3d_4(hm)
         mask=F.dropout(F.sigmoid(self.c_m(hm)),ratio=0.5)
 
         return F.tanh(h),mask
 
-    def background(self,x1,x2,x3):
-        h=self.cbr2d_4(F.concat([x3,x3]))
-        h=self.cbr2d_5(F.concat([h,x2]))
-        h=self.cbr2d_6(F.concat([h,x1]))
+    def background(self,z):
+        h=self.cbr2d_4(z)
+        h=self.cbr2d_5(h)
+        h=self.cbr2d_6(h)
         h=self.cbr2d_7(h)
+        h=self.cbr2d_8(h)
 
         return F.tanh(h)
 
-    def __call__(self,x):
-        b,_,h,w=x.shape
-        enc1,enc2,enc3=self.encode(x)
+    def __call__(self,z):
+        b = z.shape[0]
+        h = 64
+        w = 64
+        #enc1,enc2,enc3=self.encode(x)
 
-        fg,mask=self.foreground(enc3)
+        z = F.relu(self.l0(z)).reshape(b, 512, 4, 4)
+
+        fg,mask=self.foreground(z)
         mask=F.tile(mask,(1,3,1,1,1))
 
-        bg=self.background(enc1,enc2,enc3)
+        bg=self.background(z)
         bg=bg.reshape(b,3,1,h,w)
         bg=F.tile(bg,(1,1,16,1,1))
 
@@ -174,7 +183,8 @@ class Discriminator(Chain):
             self.cbr3d_2=CBR3D_dis(base*2,base*4,down=True,activation=F.leaky_relu)
             self.cbr3d_21=CBR3D_dis(base*4,base*4,activation=F.leaky_relu)
             self.cbr3d_3=CBR3D_dis(base*4,base*8,down=True,activation=F.leaky_relu)
-            self.cbr3d_4=L.Linear(None,1,initialW=w)
+            self.cbr3d_adv=L.Linear(None,1,initialW=w)
+            self.cbr3d_cls = L.Linear(None, 2,initialW=w)
 
     def __call__(self,x):
         h=self.cbr3d_0(x)
@@ -183,10 +193,11 @@ class Discriminator(Chain):
         h=self.cbr3d_11(h)
         h=self.cbr3d_2(h)
         h=self.cbr3d_21(h)
-        h=self.cbr3d_3(h)
-        h=self.cbr3d_4(h)
+        hout=self.cbr3d_3(h)
+        hadv=self.cbr3d_adv(hout)
+        hcls=self.cbr3d_cls(hout)
 
-        return h
+        return hadv
 
 class VGG(Chain):
     def __init__(self):
--- Two-Stream/train.py
+++ Two-Stream/train.py
@@ -28,43 +28,15 @@ def prepare_dataset(filename):
 
         return hr_image
 
-def prepare_test(filename):
-    image = cv.imread(filename)
-    if image is not None:
-        image = cv.resize(image,(128,128),interpolation=cv.INTER_CUBIC)
-        height,width = image.shape[0],image.shape[1]
-
-        leftdown = int(height/2)-20
-        leftup = int(height/2)+10
-        rightdown = int(width/2)-35
-        rightup = int(width/2)-5
-        lefteye = image[leftdown:leftup, rightdown:rightup]
-        lefteye = cv.resize(lefteye,(32,32),interpolation=cv.INTER_CUBIC)
-        lefteye = lefteye[:,:,::-1].transpose(2,0,1)
-        lefteye = (lefteye - 127.5) / 127.5
-        leftlist = [leftdown, leftup, rightdown, rightup]
-
-        leftdown = int(height/2)-20
-        leftup = int(height/2)+10
-        rightdown = int(width/2)+5
-        rightup = int(width/2)+35
-        righteye = image[leftdown:leftup, rightdown:rightup]
-        righteye = cv.resize(righteye,(32,32),interpolation=cv.INTER_CUBIC)
-        righteye = righteye[:,:,::-1].transpose(2,0,1)
-        righteye = (righteye - 127.5) / 127.5
-        rightlist = [leftdown, leftup, rightdown, rightup]
-
-        image = image[:,:,::-1]
-        return image,lefteye, leftlist, righteye, rightlist
-
 parser=argparse.ArgumentParser(description="Two-Stream")
 parser.add_argument("--framesize",default=16,type=int,help="frame size")
 parser.add_argument("--epoch",default=1000,type=int,help="the number of epochs")
 parser.add_argument("--iterations",default=2000,type=int,help="the number of iterations")
 parser.add_argument("--interval",default=5,type=int,help="the interval of snapshot")
 parser.add_argument("--weight",default=5.0,type=float,help="the weight of grad loss")
-parser.add_argument("--ndis",default=1,type=int,help="the number of discriminator update")
-parser.add_argument("--size",default=32,type=int,help="the image width")
+parser.add_argument("--ndis",default=2,type=int,help="the number of discriminator update")
+parser.add_argument("--size",default=64,type=int,help="the image width")
+parser.add_argument("--batchsize", default=2, type=int, help="batch size")
 
 args=parser.parse_args()
 framesize=args.framesize
@@ -74,26 +46,15 @@ interval=args.interval
 weight=args.weight
 n_dis=args.ndis
 size=args.size
+batchsize = args.batchsize
 
 outdir="./output"
 if not os.path.exists(outdir):
     os.mkdir(outdir)
 
-image_path="/image/"
+image_path="./syani"
 image_list=os.listdir(image_path)
 list_len=len(image_list)
-
-lefteye_box = []
-righteye_box = []
-image_name="./test.png"
-test, lefteye, leftlist, righteye, rightlist =prepare_test(image_name)
-lefteye_box.append(lefteye)
-righteye_box.append(righteye)
-frames1=chainer.as_variable(xp.array(lefteye_box).astype(xp.float32))
-frames2=chainer.as_variable(xp.array(righteye_box).astype(xp.float32))
-x1=frames1[0].reshape(1,3,size,size)
-x2=frames2[0].reshape(1,3,size,size)
-xtest=F.concat([x1,x2],axis=0)
 
 generator=Generator()
 generator.to_gpu()
@@ -103,58 +64,57 @@ discriminator=Discriminator()
 discriminator.to_gpu()
 dis_opt=set_optimizer(discriminator)
 
+ztest = xp.random.uniform(-1,1,(batchsize, 128)).astype(xp.float32)
+ztest = chainer.as_variable(ztest)
+
 for epoch in range(epochs):
     sum_gen_loss=0
     sum_dis_loss=0
     for batch in range(0,iterations,framesize):
-        for _ in range(n_dis):
-            lefteye_box = []
-            righteye_box = []
-            rnd=np.random.randint(list_len)
-            dir_path=image_path+image_list[rnd]
+        batch_box = []
+        for _ in range(batchsize):
+            frame_box = []
             for index in range(framesize):
-                filename=dir_path + "/lefteye_" + str(index) + ".png"
+                start_frame = np.random.randint(1, 1000 - framesize)
+                filename=image_path + "tenka_" + str(start_frame + index) + ".png"
                 frame=prepare_dataset(filename)
-                lefteye_box.append(frame)
-                filename=dir_path+"/righteye_"+str(index)+".png"
-                frame=prepare_dataset(filename)
-                righteye_box.append(frame)
+                frame_box.append(frame)
 
-            frames1=chainer.as_variable(xp.array(lefteye_box).astype(xp.float32))
-            frames2=chainer.as_variable(xp.array(righteye_box).astype(xp.float32))
-            x1=frames1[0].reshape(1,3,size,size)
-            x2=frames2[0].reshape(1,3,size,size)
-            x=F.concat([x1,x2],axis=0)
-            target1=frames1.transpose(1,0,2,3).reshape(1,3,framesize,size,size)
-            target2=frames2.transpose(1,0,2,3).reshape(1,3,framesize,size,size)
-            target=F.concat([target1,target2],axis=0)
-            target=F.concat([target, target],axis=2)
+            frames = xp.array(frame_box).astype(xp.float32).transpose(1,0,2,3).reshape(1,3,16,64,64)
+            batch_box.append(frames)
 
-            y=generator(x)
-            y=F.concat([y,y],axis=2)
-            dis_x=discriminator(y)
-            dis_t=discriminator(target)
+        t = chainer.as_variable(xp.concatenate(batch_box))
+        z = xp.random.uniform(-1,1,(batchsize, 128)).astype(xp.float32)
+        z = chainer.as_variable(z)
 
-            y.unchain_backward()
+        y=generator(z)
+        dis_x=discriminator(y)
+        dis_t=discriminator(t)
 
-            dis_loss=F.mean(F.softplus(-dis_t))+F.mean(F.softplus(dis_x))
+        y.unchain_backward()
 
-            discriminator.cleargrads()
-            dis_loss.backward()
-            dis_opt.update()
-            dis_loss.unchain_backward()
+        dis_loss=F.mean(F.softplus(-dis_t))+F.mean(F.softplus(dis_x))
 
-        y=generator(x)
-        y=F.concat([y,y],axis=2)
+        discriminator.cleargrads()
+        dis_loss.backward()
+        dis_opt.update()
+        dis_loss.unchain_backward()
+
+        z = xp.random.uniform(-1,1,(batchsize, 128)).astype(xp.float32)
+        z = chainer.as_variable(z)
+
+        y=generator(z)
         dis_x=discriminator(y)
 
         gen_loss=F.mean(F.softplus(-dis_x))
-        gen_loss+=weight*F.mean_absolute_error(y,target)
 
         generator.cleargrads()
         gen_loss.backward()
         gen_opt.update()
         gen_loss.unchain_backward()
+
+        for p in discriminator.params():
+            p.data = xp.clip(p.data,-0.01,0.01)
 
         sum_dis_loss+=dis_loss.data.get()
         sum_gen_loss+=gen_loss.data.get()
@@ -164,19 +124,13 @@ for epoch in range(epochs):
             pylab.rcParams['figure.figsize'] = (16.0,16.0)
             pylab.clf()
             with chainer.using_config("train",False):
-                y=generator(xtest)
+                y=generator(ztest)
             y=y.data.get()
-            yleft=y[0].transpose(1,0,2,3)
-            yright=y[1].transpose(1,0,2,3)
+            y=y[0].transpose(1,0,2,3)
             for i_ in range(framesize):
-                tmp_left = (np.clip(yleft[i_]*127.5+127.5,0,255)).transpose(1,2,0).astype(np.uint8)
-                tmp_right = (np.clip(yright[i_]*127.5+127.5,0,255)).transpose(1,2,0).astype(np.uint8)
-                tmp_left = cv.resize(tmp_left,(30,30),interpolation=cv.INTER_CUBIC)
-                tmp_right = cv.resize(tmp_right,(30,30),interpolation=cv.INTER_CUBIC)
-                test[leftlist[0]:leftlist[1], leftlist[2]:leftlist[3]] = tmp_left
-                test[rightlist[0]:rightlist[1], rightlist[2]:rightlist[3]] = tmp_right
+                tmp = np.clip(y[i_]*127.5 + 127.5, 0,255).transpose(1,2,0).astype(np.uint8)
                 pylab.subplot(4,4,i_+1)
-                pylab.imshow(test)
+                pylab.imshow(tmp)
                 pylab.axis('off')
                 pylab.savefig('%s/visualize_%d.png'%(outdir, epoch))
 
